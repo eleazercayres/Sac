d42a5c22fafe48dfc4e9a2ceda957e6dc8b66d08
eleazer.mota@gmail.com <eleazer.mota@gmail.com>
2017-10-03 20:10:34 -0300

    First commit

---
+++ sac-web-bff/src/main/java/com/olx/sac/sacwebbff/App.java
@@ -0,0 +1,13 @@
+package com.olx.sac.sacwebbff;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
