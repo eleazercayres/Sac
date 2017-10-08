7765d6339a22b574b53d30f02f7fbfa3e2ee3bbf
eleazer.mota@gmail.com <eleazer.mota@gmail.com>
2017-10-08 00:45:35 -0300

    Juncao de projetos

--- sac-service/src/main/java/com/olx/sac/sacservice/service/StateService.java
+++
@@ -1,9 +0,0 @@
-package com.olx.sac.sacservice.service;
-
-import com.olx.sac.sacservice.exception.CallServiceException;
-import com.olx.sac.sacserviceapi.attendance.StateDTO;
-
-public interface StateService {
-
-	public StateDTO findOnByUf(String uf) throws CallServiceException;
-}
