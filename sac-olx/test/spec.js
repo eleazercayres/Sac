481e059a966eb8daf4568933a6fcc589b47e29e3
eleazer mota <eleazer.mota@mobicare.com.br>
2017-10-09 12:32:20 -0300

    subindo testes de front

---
+++ sac-olx/test/spec.js
@@ -0,0 +1,24 @@
+// spec.js
+describe('Protractor Test App', function() {
+  it('should add one and two', function() {
+    browser.get('http://localhost:8087/index.html#/');
+
+    element(by.cssContainingText('type', 'Telefone')).click();
+    element(by.cssContainingText('type', 'Telefone')).click();
+    element(by.cssContainingText('type', 'Telefone')).click();
+
+    element(by.model('detalhes')).sendKeys("Teste detalhes");
+
+    element(by.id('submit')).click();
+
+    expect(element(by.binding('success_message')).getText()).
+        toEqual('Success'); 
+
+    element(by.id('rel')).click();
+
+    var list = element.all(by.repeater('call in calls'));
+
+    expect(list.count()).toEqual(12);
+
+  });
+});
